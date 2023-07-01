abc1dae353fa297887d1a406f6bb904b549b7c7d
kira-yangwenxu <32013388+kira-yangwenxu@users.noreply.github.com>
2023-07-01 16:26:49 +0800

    Add files via upload

---
+++ test11/Test5.vue
@@ -0,0 +1,34 @@
+<script>
+import lessons from "../../data/lessons.js";
+export default {
+    data() {
+       return {
+           lessons
+       }
+    }
+}
+</script>
+
+<template>
+    <div class="ClassList">
+         <div v-for="(lesson,key) in lessons"  :key="key">
+           <span :class="{'is-delete' : lesson.isDelete}">{{lesson.name}}</span>
+           <button @click="lesson.isDelete = !lesson.isDelete">{{lesson.isDelete ? '取消': '删除'}}</button>
+         </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .ClassList{
+      div{
+        display: flex;
+        justify-content:space-between;
+      }
+      span{
+        line-height: 2.5;
+        &.is-delete {
+           text-decoration:line-through;
+        }
+      }
+    }
+</style>
