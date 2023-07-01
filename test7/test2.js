74b216d02b55cd4184f6bfe82d62467a2fe3eed7
kira-yangwenxu <32013388+kira-yangwenxu@users.noreply.github.com>
2023-07-01 16:25:27 +0800

    Add files via upload

---
+++ test7/test2.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			name:'伊吕波',
+			num:1,
+			tip:''
+		}
+	},
+	computed: {
+			error : {
+				get() {
+					const  message = this.num == 0 ? '不能小于0' : this.num == 10 ?'不能大于10' : ''
+					if (message) return this.tip + message
+				},
+				set (tip) {
+					this.tip = tip
+				}
+			}
+	},
+	methods : {
+		add(event) {
+			if (this.num < 10) {
+				this.error = '提示'
+				this.num ++
+			}
+		},
+		des(event) {
+			if (this.num > 0) {
+				this.error ='警告'
+				this.num--
+			}
+		}
+	}
+})
+const mv = app.mount('#app')
---
+++ test7/test4.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			name:'伊吕波',
+			num:1,
+			tip:''
+		}
+	},
+	computed : {
+		error: {
+			get() {
+				const message = this.num ==0 ? '不能小于0' : this.num ==10 ? '不能大于10' : ''
+				if(message) return this.tip + message
+			},
+			set(tip) {
+				this.tip = tip
+			}
+		}
+	},
+	methods : {
+		add(event) {
+			if (this.num < 10) {
+				this.num++
+				this.error = '提示'
+			}
+		},
+		des(event) {
+			if (this.num > 0) {
+				this.num --
+				this.error = '警告'
+			}
+		}
+	}
+})
+const vm = app.mount('#app')
