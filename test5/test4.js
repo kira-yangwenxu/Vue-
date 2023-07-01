ff4f4d3b95ed681b7f83e85e18efdfa5c8100b41
kira-yangwenxu <32013388+kira-yangwenxu@users.noreply.github.com>
2023-07-01 16:23:57 +0800

    Add files via upload

---
+++ test5/test4.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: '伊吕波',
+            id:'yilvbo',
+            n:'1',
+            href:'value',
+            value:'www.yilvbo.com'
+        }
+    }
+})
+
+const vm = app.mount('#app')
+
+setTimeout(() => {
+    vm.id = '白子'
+    vm.n = 100
+    vm.name = '优香'
+    vm.href = 'href'
+}, 3000)
